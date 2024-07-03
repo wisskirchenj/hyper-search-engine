e257c378b5e1b703febbcecb32bb27e2be52246b
wisskirchenj <wisskirchen.juergen@gmail.com>
2024-07-03 11:32:17 +0200

    fix dirpath for tests

--- test/test_corpus_load.py
+++ test/test_corpus_load.py
@@ -7,7 +7,7 @@ class TestCorpusLoad(unittest.TestCase):
 
     @patch("builtins.print", wrap=print)
     def test_file_stats(self, print_spy):
-        search.file_stats.PATH = '../corpus'
+        search.file_stats.PATH = 'corpus'
         search.file_stats.FileStats().main()
         print_spy.assert_any_call('Branching.txt: 381')
         print_spy.assert_any_call('Intro to text representation.txt: 725')
